a8c26becf6478a38f9ac528ff0235613d526aff3
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-04-13 16:24:28 -0300

    begin to index.js

--- E1- CLI/index.js
+++ E1- CLI/index.js
@@ -1,1 +1,30 @@
 // lógica del script
+
+import {get, save} from "./fileMethods.js";
+import inquirer from "inquirer";
+import {promptNewGasto} from "./userPrompts.js";
+
+
+const main = async () => {
+    let run = true;
+    while (run) {
+        const action = await inquirer.prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "Que quieres hacer?",
+                choices: [
+                    { value:1, name: "Añadir un nuevo gasto"},
+                    { value:2, name: "Ver todos los gastos"},
+                    { value:3, name: "Salir"},
+                    
+                ],
+
+            },
+        ]);
+            
+        
+    }            
+}
+
+
--- E1- CLI/userPrompts.js
+++ E1- CLI/userPrompts.js
@@ -1,1 +1,40 @@
 // menú para el inquirer
+
+import inquirer from "inquirer";
+import DatePrompt from "inquirer-date-prompt";
+
+inquirer.registerPrompt("date", DatePrompt); // mtodo que permite poner el TIPO de pregunta para que el user ingrese informacionn en el formato que quiera. 
+
+// es async para que se ejecute SOLO despues de que se resuelva el inquirer
+export async function promptNewGasto() {
+    return await inquirer.prompt(newGastoPrompts);
+
+}
+
+const newGastoPrompts = [
+    { //así debemos armar todas las opciones del menu, según ducamentación
+        type: "input",
+        name: "concepto",
+        message: "Concepto del gasto: "
+    },
+    {
+        type: "date",
+        name: "fecha",
+        message: "Fecha del gasto: ",
+        format: {month:"short", hour:undefined, minute:undefined}
+    },
+    {
+        type: "date",
+        name: "valor",
+        message: "Valor del gasto: ",
+        validate: (value) => {
+            const valid = !isNaN(parseFloat(value));
+            return valid || "Por favor ingresa un valor valido";
+        }
+    },
+    {
+        type: "input",
+        name: "observaciones",
+        message: "Observaciones del gasto: "
+    }
+];
